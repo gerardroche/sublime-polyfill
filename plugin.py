3a8eca931f81b204131f233e012ff73cddaeb290
Gerard Roche <gerardroche@users.noreply.github.com>
2017-10-12 15:03:41 +0100

    Fix Sort User Settings
    * Lists should be unique
    * Add sort for added_words

--- plugin.py
+++ plugin.py
@@ -346,28 +346,39 @@ class SortUserSettingsCommand(WindowCommand):
     def run(self):
         preferences = _load_preferences()
 
+        # added_words
+        added_words = preferences.get('added_words', [])
+        added_words = list(set(added_words))
+        added_words.sort()
+        preferences.set('added_words', added_words)
+
         # ignored_packages
         ignored_packages = preferences.get('ignored_packages', [])
+        ignored_packages = list(set(ignored_packages))
         ignored_packages.sort()
         preferences.set('ignored_packages', ignored_packages)
 
         # ignored_words
         ignored_words = preferences.get('ignored_words', [])
+        ignored_words = list(set(ignored_words))
         ignored_words.sort()
         preferences.set('ignored_words', ignored_words)
 
         # index_exclude_patterns
         index_exclude_patterns = preferences.get('index_exclude_patterns', [])
+        index_exclude_patterns = list(set(index_exclude_patterns))
         index_exclude_patterns.sort()
         preferences.set('index_exclude_patterns', index_exclude_patterns)
 
         # file_exclude_patterns
         file_exclude_patterns = preferences.get('file_exclude_patterns', [])
+        file_exclude_patterns = list(set(file_exclude_patterns))
         file_exclude_patterns.sort()
         preferences.set('file_exclude_patterns', file_exclude_patterns)
 
         # folder_exclude_patterns
         folder_exclude_patterns = preferences.get('folder_exclude_patterns', [])
+        folder_exclude_patterns = list(set(folder_exclude_patterns))
         folder_exclude_patterns.sort()
         preferences.set('folder_exclude_patterns', folder_exclude_patterns)
 
