cc61b5d7cfe11f694dbe27bf16411bda35ed0593
Gerard Roche <gerardroche@users.noreply.github.com>
2020-01-20 17:14:58 +0000

    :metal: Remove windowing commands

--- ui.py
+++ ui.py
@@ -16,33 +16,6 @@ def _save_preferences():
     return save_settings('Preferences.sublime-settings')
 
 
-class ClearWindowCommand(sublime_plugin.WindowCommand):
-
-    def run(self):
-        if self.window.is_sidebar_visible():
-            self.window.set_sidebar_visible(False)
-
-        if self.window.is_minimap_visible():
-            self.window.set_minimap_visible(False)
-
-        if self.window.is_menu_visible():
-            self.window.set_menu_visible(False)
-
-        if self.window.is_status_bar_visible():
-            self.window.set_status_bar_visible(False)
-
-        self.window.run_command('resize_groups_almost_equally')
-
-        preferences = _load_preferences()
-        preferences.set('indent_guide_options', [])
-        preferences.set('line_numbers', False)
-        preferences.set('draw_white_space', 'selection')
-        preferences.set('rulers', [])
-        _save_preferences()
-
-        self.window.run_command('sort_user_settings')
-
-
 class EnableColorSchemeCommand(sublime_plugin.ApplicationCommand):
 
     def run(self):
@@ -208,67 +181,3 @@ class PolyfillSetLayoutCommand(sublime_plugin.WindowCommand):
 
         view = self.window.active_view_in_group(active_group_before)
         self.window.set_view_index(view, self.window.active_group(), 0)
-
-
-class ResetWindowCommand(sublime_plugin.WindowCommand):
-
-    def run(self):
-
-        self.window.run_command('reset_font_size')
-
-        view = self.window.active_view()
-        font_size = view.settings().get('font_size_default') if view else None
-        if font_size:
-            preferences = _load_preferences()
-            preferences.set('font_size', font_size)
-            _save_preferences()
-
-        if not self.window.is_sidebar_visible():
-            self.window.set_sidebar_visible(True)
-
-        if not self.window.is_minimap_visible():
-            self.window.set_minimap_visible(True)
-
-        if not self.window.is_menu_visible():
-            self.window.set_menu_visible(True)
-
-        if not self.window.is_status_bar_visible():
-            self.window.set_status_bar_visible(True)
-
-        self.window.run_command('resize_groups_almost_equally')
-
-
-class ResizeGroupsAlmostEquallyCommand(sublime_plugin.WindowCommand):
-    """
-    Resize groups equally.
-
-    Make all groups (almost) equally high and wide, but use 'winheight' and
-    'winwidth' for the current window.  Windows with 'winfixheight' set keep
-    their height and windows with 'winfixwidth' set keep their width.
-    @xxx winheight option
-    @xxx winwidth option
-    @xxx winfixheight option
-    @xxx winfixwidth option
-    """
-
-    def run(self):
-        layout = self.window.layout()
-        col_count = len(layout['cols'])
-        row_count = len(layout['rows'])
-
-        def equalise(count):
-            size = round(1.0 / (count - 1), 2)
-            vals = [0.0]
-            for i in range(1, count - 1):
-                vals.append(round(size * i, 2))
-            vals.append(1.0)
-            return vals
-
-        if col_count > 2:
-            layout['cols'] = equalise(col_count)
-
-        if row_count > 2:
-            layout['rows'] = equalise(row_count)
-
-        if col_count > 2 or row_count > 2:
-            self.window.set_layout(layout)
